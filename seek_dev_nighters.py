93cc6e05fdef69171447c17b33423bb137603141
Roman Yakovlev <yakovlevroman8@gmail.com>
2016-12-13 02:11:28 +0700

    уменьшил количество запросов
    информация о количестве страниц запрашивается вместе с информацией о юзерах

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -6,32 +6,36 @@ from pytz import timezone
 
 
 def load_attempts():
-    request_data = requests.get("http://devman.org/api/challenges/solution_attempts/").json()
-    pages = request_data["number_of_pages"]
-    for page in range(pages):
-        get_params = {"page": page+1}
-        users_data = requests.get("http://devman.org/api/challenges/solution_attempts/",
-                                  params=get_params).json()
+    page = 1
+    get_params = {"page": page}
+    users_data = requests.get("http://devman.org/api/challenges/solution_attempts/",
+                              params=get_params).json()
+    pages = users_data["number_of_pages"]
+    first_page = 1
+    for page in range(first_page, pages):
         for user in users_data["records"]:
             yield {
                 'username': user["username"],
                 'timestamp': user["timestamp"],
                 'timezone': user["timezone"],
             }
+        get_params = {"page": page+1}
+        users_data = requests.get("http://devman.org/api/challenges/solution_attempts/",
+                                  params=get_params).json()
 
 
 def get_midnighters():
-    hours_for_owls = [0,1,2,3,4,5]
-    arr = []
+    owls_list = []
     for user in load_attempts():
         if user["timestamp"] is None:
             continue
         user_timezone =  user["timezone"]
         date = datetime.utcfromtimestamp(user["timestamp"])
         hour = timezone(user_timezone).fromutc(date).hour
-        if hour in hours_for_owls:
-            arr.append(user["username"])
-    return set(arr)
+        midnight, six_hours = 0, 6
+        if hour >= midnight and hour < six_hours:
+            owls_list.append(user["username"])
+    return set(owls_list)
 
 
 def output_users(users_list):
