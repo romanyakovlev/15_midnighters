e403b69bc3dc993f1d8613e17bb905881e8dc40b
Roman Yakovlev <yakovlevroman8@gmail.com>
2016-12-21 01:50:19 +0700

    сделал yield в yield-е
    Вынес yield в отдельную функцию.

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -5,28 +5,29 @@ from datetime import datetime
 from pytz import timezone
 
 
-def load_attempts():
-    first_page = 1
-    get_params = {"page": first_page}
-    users_data = requests.get("http://devman.org/api/challenges/solution_attempts/",
-                              params=get_params).json()
-    pages = users_data["number_of_pages"]
-    for user in users_data["records"]:
-            yield {
-                'username': user["username"],
-                'timestamp': user["timestamp"],
-                'timezone': user["timezone"],
-            }
-    for page in range(first_page, pages):
-        get_params = {"page": page+1}
-        users_data = requests.get("http://devman.org/api/challenges/solution_attempts/",
-                                  params=get_params).json()
+def create_generator(users_data):
         for user in users_data["records"]:
             yield {
                 'username': user["username"],
                 'timestamp': user["timestamp"],
                 'timezone': user["timezone"],
             }
+
+
+def load_attempts():
+    first_page = 1
+    get_params = {"page": first_page}
+    users_data = requests.get("http://devman.org/api/challenges/solution_attempts/",
+                              params=get_params).json()
+    pages = users_data["number_of_pages"]
+    for user in create_generator(users_data):
+        yield user
+    for page in range(first_page, pages):
+        get_params = {"page": page+1}
+        users_data = requests.get("http://devman.org/api/challenges/solution_attempts/",
+                                  params=get_params).json()
+        for user in create_generator(users_data):
+            yield user
 
 
 def get_midnighters():
@@ -38,8 +39,7 @@ def get_midnighters():
         date = datetime.utcfromtimestamp(user["timestamp"])
         hour = timezone(user_timezone).fromutc(date).hour
         midnight, six_hours = 0, 6
-        if hour >= midnight and hour < six_hours:
-            owls_list.append(user["username"])
+        owls_list.append(user["username"])
     return set(owls_list)
 
 
